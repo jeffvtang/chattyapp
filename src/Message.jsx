ac6ad5fbdad6cc465792f5925dc6ceef25c765ea
Jeff <jeff.tang@live.com>
2018-06-13 21:22:11 -0400

    fixed css issue

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 
-
 const Message = ({ message, i }) => (
-  <div className="message" key={message.id}>
+  <div>
+    {/* <span className="message-username" style={{ color: message.userColor }}>
+      Not Bob
+    </span>
+    <span className="message-content">Dummy Text</span> */}
     {message.type == "incomingMessage" && (
-      <div>
-        <span className="message-username" style={{color: message.userColor}}>{message.username}</span>
+      <div className="message" key={message.id}>
+        <span className="message-username" style={{ color: message.userColor }}>
+          {message.username}
+        </span>
         {/* <span className="message-username">{message.username}</span> */}
         <span className="message-content">{message.content}</span>
       </div>
     )}
     {message.type == "incomingNotification" && (
-      <div className="message system">
+      <div className="message system" key={message.id}>
         {message.oldName} changed their name to {message.newName}
       </div>
     )}
