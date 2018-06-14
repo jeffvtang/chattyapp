d2384dbd23baaca59811ee41247781ae17e4e579
Jeff <jeff.tang@live.com>
2018-06-13 20:05:07 -0400

    removed comments

--- src/App.jsx
+++ src/App.jsx
@@ -44,46 +44,16 @@ class App extends Component {
            this.setState({messages: messages})
           break;
         case "incomingNotification":
-          // console.log(dataFromSocket);
           const notification = this.state.messages.concat(dataFromSocket);
           this.setState({ messages: notification });
           break;
         case "userCountChanged":
           console.log(dataFromSocket);
           this.setState({ users: dataFromSocket });
-          // console.log("entire state", this.state);
-          // console.log("users", this.state.users);
           break;
       }
 
-      // if (dataFromSocket.type == "incomingMessage") {
-      //   console.log("dataFromSocket", dataFromSocket);
-      //   const messages = this.state.messages.concat(dataFromSocket);
-      //   this.setState({ messages: messages });
-      // } else if (dataFromSocket.type == "incomingNotification") {
-      //   console.log(dataFromSocket);
-      //   const messages = this.state.messages.concat(dataFromSocket);
-      //   this.setState({ messages: messages });
-      // } else if (dataFromSocket.type == "userCountChanged") {
-      //   console.log(dataFromSocket);
-      //   this.setState({ users: dataFromSocket})
-      //   console.log('entire state', this.state)
-      //   console.log('users', this.state.users)
-      // }
     });
-
-    /*    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {
-        username: "Michelle",
-        content: "Hello there!"
-      };
-      const messages = this.state.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages });
-    }, 3000); */
   }
   enterMessage = (user, content) => {
     console.log(user);
@@ -96,8 +66,6 @@ class App extends Component {
     this.socket.send(JSON.stringify(messagetoSocket));
   };
   enterName = name => {
-    // console.log(content);
-    // console.log("received", name);
     const nametoSocket = {
       oldName: this.state.currentUser.name,
       newName: name,
@@ -107,7 +75,6 @@ class App extends Component {
       name: name,
       userColor: this.state.currentUser.userColor
     };
-    // console.log("state", nametoSocket);
     this.socket.send(JSON.stringify(nametoSocket));
     this.setState({ currentUser: nametoState });
   };
@@ -117,7 +84,6 @@ class App extends Component {
     return (
       <div>
         <NavBar userCount={users.userCount} />
-        {/* <h1>Hello React :)</h1> */}
         <MessageList messages={messages} userColor={currentUser.userColor} />
         <ChatBar
           currentUser={currentUser}
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,17 +1,5 @@
 import React, { Component } from "react";
 
-// class Messages extends Component {
-//   render() {
-//     const messages = this.props.messages.map((message, i) => (
-//       <div className="message" key={i}>
-//         <span className="message-username">{message.username}</span>
-//         <span className="message-content">{message.content}</span>
-//       </div>
-//     ));
-//     console.log(messages);
-//     return <div>{messages}</div>;
-//   }
-// }
 
 const Message = ({ message, userColor, i }) => (
   <div className="message" key={message.id}>
@@ -29,9 +17,5 @@ const Message = ({ message, userColor, i }) => (
   </div>
 );
 {
-  /* <div className="message system">
-  {message.oldName} changed their name to {message.newName}
-</div>; */
-}
 
 export default Message;
